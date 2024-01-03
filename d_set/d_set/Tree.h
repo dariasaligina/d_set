d85fc8de183b30b767cb1a4954efe3c4f3a9ebd8
dariasaligina <146203741+dariasaligina@users.noreply.github.com>
2024-01-03 18:15:06 +0300

    Add files via upload

---
+++ d_set/d_set/Tree.h
@@ -0,0 +1,118 @@
+#pragma once
+#include<vector>
+
+using namespace std;
+
+template < typename T > class d_set;
+
+template < typename T > class Tree {
+private:
+
+    int prior, size;
+    T key;
+    Tree < T >* l = 0, * r = 0;
+
+    friend class d_set < T >;
+
+    int update_size() {
+        int s = 1;
+        if (l) s += l->update_size();
+        if (r) s += r->update_size();
+        size = s;
+        return s;
+    }
+    Tree() {}
+
+    Tree(T _key) {
+        key = _key, prior = rand(), l = 0, r = 0;
+    }
+
+    friend void split(Tree < T >* t, T key, Tree < T >*& l, Tree < T >*& r) {
+        if (!t)
+            l = r = NULL;
+        else if (key < t->key) {
+            split(t->l, key, l, t->l);
+            r = t;
+        }
+        else {
+            split(t->r, key, t->r, r);
+            l = t;
+        }
+    }
+
+    friend void insert_nd(Tree < T >*& t, Tree < T >* it) {
+        if (!t)
+            t = it;
+        else if (it->prior > t->prior) {
+            split(t, it->key, it->l, it->r);
+            t = it;
+        }
+        else
+            insert_nd(it->key < t->key ? t->l : t->r, it);
+    }
+
+    friend void merge(Tree < T >*& t, Tree < T >* l, Tree < T >* r) {
+        if (!l || !r)
+            t = l ? l : r;
+        else if (l->prior > r->prior) {
+            merge(l->r, l->r, r);
+            t = l;
+        }
+        else {
+            merge(r->l, l, r->l);
+            t = r;
+        }
+    }
+
+    friend void errase_nd(Tree < T >*& t, T key) {
+        if (!t) return;
+        if (t->key == key) {
+            merge(t, t->l, t->r);
+            return;
+        }
+        else
+            errase_nd(key < t->key ? t->l : t->r, key);
+    }
+
+    friend bool is_in_nd(Tree < T >* t, T key) {
+        if (!t) return 0;
+        if (t->key == key)
+            return 1;
+        else
+            return is_in_nd(key < t->key ? t->l : t->r, key);
+    }
+
+    friend void to_vector_nd(Tree < T >* root, vector < T >& ans) {
+        if (root->l) to_vector_nd(root->l, ans);
+        ans.push_back(root->key);
+        if (root->r) to_vector_nd(root->r, ans);
+    }
+
+    Tree < T >* copy() {
+        Tree < T >* ans = new Tree(key);
+        ans->key = key;
+        ans->prior = prior;
+        ans->size = size;
+
+        if (l)
+            ans->l = l->copy();
+        if (r)
+            ans->r = r->copy();
+        return ans;
+    }
+
+    void destruct() {
+        if (this) {
+            if (l) {
+                l->destruct();
+                l = 0;
+            }
+            if (r) {
+                r->destruct();
+                r = 0;
+            }
+            free(this);
+        }
+    }
+
+};
---
+++ d_set/d_set/d_set.h
@@ -0,0 +1,107 @@
+#pragma once
+#include "Tree.h"
+
+
+template<typename T> class d_set {
+public:
+
+	~d_set() {
+		if (root)
+			root->destruct();
+		root = 0;
+	}
+
+	d_set() {
+		root = 0;
+	}
+
+	d_set(const d_set& st) {
+		this->root = st.root->copy();
+	}
+
+	void operator=(d_set st) {
+		root = st.root->copy();
+	}
+
+	void insert(T n) {
+		if (!(is_in(n))) {
+			insert_nd(root, new Tree<T>(n));
+		}
+	}
+
+	void errase(T n) {
+		errase_nd(root, n);
+	}
+
+	vector<T> to_vector() {
+		vector<T>ans;
+		if (root)
+			to_vector_nd(root, ans);
+		return ans;
+	}
+
+	d_set operator +(d_set& r) {
+		d_set ans = this->copy();
+		vector<T> v = r.to_vector();
+		for (auto i : v) {
+			ans.insert(i);
+		}
+		return ans;
+	}
+
+	d_set operator&(d_set& s2) {
+		d_set st;
+		vector<T> v = to_vector();
+		for (auto i : v) {
+			if (s2.is_in(i)) {
+				st.insert(i);
+			}
+		}
+		return st;
+	}
+
+	d_set operator-(d_set& r) {
+		vector<T> v = r.to_vector();
+		d_set ans = this->copy();
+		for (auto i : v) {
+			ans.errase(i);
+		}
+		return ans;
+	}
+
+	int size() {
+		return root->update_size();
+	}
+
+	bool is_in(T n) {
+		return is_in_nd(root, n);
+	}
+
+	bool is_in(d_set& st) {
+		vector<T> v = st.to_vector();
+		for (auto i : v) {
+			if (!is_in(i)) return 0;
+		}
+		return 1;
+	}
+
+	d_set copy() {
+		d_set ans;
+		ans.root = root->copy();
+		return ans;
+	};
+
+	void clear() {
+		if (root) root->del();
+		root = 0;
+	}
+
+	friend void swap(d_set& s1, d_set& s2) {
+		Tree<T>* t1 = s1.root;
+		s1.root = s2.root;
+		s2.root = t1;
+	}
+
+private:
+	Tree<T>* root = 0;
+};
